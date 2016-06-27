ead648b1308860dbf0d09ec2a6f8d895f55df715
Jamie Olsen <jwolsen@mmm.com>
2016-06-27 12:48:19 -0600

    merge to commit

--- FlickrBrowser/app/src/main/java/org/example/android/flickrbrowser/BaseActivity.java
+++ FlickrBrowser/app/src/main/java/org/example/android/flickrbrowser/BaseActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 public class BaseActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    public static final String FLICKR_QUERY = "FLICKR_QUERY";
 
     protected Toolbar activateToolbar() {
         if(mToolbar == null) {
@@ -20,7 +21,7 @@ public class BaseActivity extends AppCompatActivity {
         return mToolbar;
     }
 
-    protected Toolbar ActivateToolbarWithHomeEnabled(){
+    protected Toolbar activateToolbarWithHomeEnabled(){
         activateToolbar();
         if(mToolbar != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
--- FlickrBrowser/app/src/main/java/org/example/android/flickrbrowser/MainActivity.java
+++ FlickrBrowser/app/src/main/java/org/example/android/flickrbrowser/MainActivity.java
@@ -1,6 +1,9 @@
 package org.example.android.flickrbrowser;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
@@ -20,19 +23,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
-//        GetRawData theRawData = new GetRawData("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,lollipop&format=json&nojsoncallback=1");
         activateToolbar();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
@@ -40,6 +31,27 @@ public class MainActivity extends BaseActivity {
 
         ProcessPhotos processPhotos = new ProcessPhotos("google",true);
         processPhotos.execute();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if(flickrRecyclerViewAdapter != null)
+        {
+            String query = getSavedPreferenceData(FLICKR_QUERY);
+            if(query.length() > 0)
+            {
+                ProcessPhotos processPhotos = new ProcessPhotos(query,true);
+                processPhotos.execute();
+            }
+        }
+    }
+
+    private String getSavedPreferenceData(String key) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        return sharedPreferences.getString(key,"");
+
     }
 
     @Override
@@ -60,9 +72,15 @@ public class MainActivity extends BaseActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if(id==R.id.menu_search){
+            Intent intent = new Intent(this, SearchActivity.class);
+            startActivity(intent);
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
+
 
     public class ProcessPhotos extends GetFlickrJsonData {
 
