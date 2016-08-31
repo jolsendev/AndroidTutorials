12ccaafa7117618c3b2bc4cdaef484ceafe9316b
Jamie Olsen <jwolsen@mmm.com>
2016-08-31 13:26:25 -0600

    created custom adapter for gridview

---
+++ PopularMovies/app/src/main/java/com/example/jamie/popularmovies/CustomMovieAdapter.java
@@ -0,0 +1,30 @@
+package com.example.jamie.popularmovies;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by A5W5NZZ on 8/31/2016.
+ */
+public class CustomMovieAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
